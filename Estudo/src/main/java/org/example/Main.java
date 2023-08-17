81493f6f0a371cc3c28f8deb13d7f0f14cae222f
CarlosQw32 <ncarlos63@gmail.com>
2023-08-16 22:17:47 -0300

    Pratica git

--- Estudo/src/main/java/org/example/Main.java
+++ Estudo/src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ import jakarta.persistence.Persistence;
 public class Main {public static void main(String[] args) throws Exception {
     var entityManagerFactory = Persistence.createEntityManagerFactory( "default");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    System.out.println("Teste");
 }
 }
