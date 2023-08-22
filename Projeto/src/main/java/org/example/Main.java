d3c2cd0f29bcb700db873e0a55f128bc09dc9f5a
CarlosQw32 <ncarlos63@gmail.com>
2023-08-21 22:58:13 -0300

    Exercico em aula

---
+++ Projeto/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class Main {public static void main(String[] args) throws Exception {
+    var entityManagerFactory = Persistence.createEntityManagerFactory( "default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    System.out.println("Teste");
+}
+}
