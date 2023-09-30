1496ab269be83da4a2bd1720b62b99610ca3a168
CarlosQw32 <ncarlos63@gmail.com>
2023-09-29 21:17:59 -0300

    feat: Seguindo padrao de insert do professor

--- Spotify/src/main/resources/META-INF/Database/load.sql
+++ Spotify/src/main/resources/META-INF/Database/load.sql
@@ -1,7 +1,11 @@
-INSERT into genre (genre_id,genre_name)values (1,'Jazz');
-INSERT into genre (genre_id,genre_name)values (2,'Rap');
-INSERT into genre (genre_id,genre_name)values (3,'Pagode ');
-INSERT into genre (genre_id,genre_name)values (4,'Sertanejo');
-INSERT into genre (genre_id,genre_name)values (5,'Funk');
+INSERT INTO ARTIST (artist_name, artist_type) VALUES ('The Weeknd', 'Pop')
+INSERT INTO ARTIST (artist_name, artist_type) VALUES ('Tyler The Creator', 'Rap')
+INSERT INTO ARTIST (artist_name, artist_type) VALUES ('Billie Eilish', 'Pop')
 
-INSERT into track (track_id,track_price,track_size,track_name) values (1,1.50,3.45,'Musica teste');
+INSERT INTO TRACK (track_name, track_length, track_size, track_price, track_language, track_producer, track_songwriters, track_released, artist_id) VALUES ('Cant Feel My Face', 180, 180, 50, 'EN', NULL, NULL, '2015-06-15',1)
+INSERT INTO TRACK (track_name, track_length, track_size, track_price, track_language, track_producer, track_songwriters, track_released, artist_id) VALUES ('Rise!', 180, 180, 50, 'EN', NULL, NULL, '2021-06-21',1)
+INSERT INTO TRACK (track_language, track_length, track_name, track_price, track_producer, track_released, track_size, track_songwriters, artist_id) VALUES ('EN', 180, 'Happier Than Ever', 50, NULL, '2021-07-30', 180, NULL,1)
+
+INSERT INTO ARTIST_TRACK (track_id, artist_id) VALUES (1,1)
+INSERT INTO ARTIST_TRACK (track_id, artist_id) VALUES (2,1)
+INSERT INTO ARTIST_TRACK (track_id, artist_id) VALUES (3,1)
