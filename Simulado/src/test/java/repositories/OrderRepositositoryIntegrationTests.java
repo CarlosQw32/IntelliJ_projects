20dc8ff92002cd7b891d531a889fbb0cd6d2aaea
CarlosQw32 <ncarlos63@gmail.com>
2023-10-16 18:01:25 -0300

    Testes 1 e 2 rodando falta finalizar o 3

--- Simulado/src/test/java/repositories/OrderRepositositoryIntegrationTests.java
+++ Simulado/src/test/java/repositories/OrderRepositositoryIntegrationTests.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class OrderRepositositoryIntegrationTests {
 
     private static EntityManagerFactory entityManagerFactory;
@@ -32,8 +35,56 @@ public class OrderRepositositoryIntegrationTests {
     @Order(1)
     public void testCreatValidOrder(){
 
-        var order = new Order();
-        var customerId faker.number().numberBetween(0,99);
-        order.setCus
+        var order = new org.example.entities.Order();
+        var customerId = faker.number().numberBetween(0,99);
+        var id = faker.number().numberBetween(0,99);
+        var statusCodeId = faker.number().numberBetween(1,5);
+        var customerComments = faker.lorem().words(new Random().nextInt(10)).toString();
+        order.setCustomer_id(customerId);
+        order.setId(id);
+        order.setStatus_code_id(statusCodeId);
+        order.setCustomer_comments(customerComments);
+
+        orederRepository.InsertOrder(order);
+
+        var savedOrder = orederRepository.GetOrderById(order.getId()).orElse(null);
+        assertNotNull(savedOrder);
+        assertEquals(order.getId(), savedOrder.getId());
+
+    }
+
+    @Test
+    @Order(2)
+    public void  testCreateInvalidOrder(){
+        //Arrange
+        var order = new org.example.entities.Order();
+
+        //Act
+        try {
+            orederRepository.InsertOrder(order);
+        }catch (Exception e){
+
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    @Order(3)
+    public void testUpdateOrder(){
+
+        var order = new org.example.entities.Order();
+        var customerId = faker.number().numberBetween(0,99);
+        var id = faker.number().numberBetween(0,99);
+        var statusCodeId = faker.number().numberBetween(1,5);
+        var customerComments = faker.lorem().words(new Random().nextInt(10)).toString();
+        order.setCustomer_id(customerId);
+        order.setId(id);
+        order.setStatus_code_id(statusCodeId);
+        order.setCustomer_comments(customerComments);
+
+        orederRepository.InsertOrder(order);
+
+
+
     }
 }
